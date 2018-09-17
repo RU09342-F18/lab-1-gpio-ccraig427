2435cdb169c432279f1f1df6607b5517bee5b603
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-17 16:24:06 -0400

    Create main.c

---
+++ Multiple Blink/MSP430G2553/main.c
@@ -0,0 +1,25 @@
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1DIR |= BIT0 | BIT6; //assigns BIT0 and BIT6 to have an output direction
+	P1SEL &= ~(BIT0 + BIT6); //assigns value of 0 to Pin1 and Pin6 in P1SEL SFR
+	int a = 0;
+	int b = 0;
+	while(1)
+	{
+	    a += 1;
+	    b += 1;
+	    __delay_cycles(50000);
+	    if (a == 25)
+	    {
+	        P1OUT ^= BIT0;
+	        a = 0;
+	    }
+	    if (b == 10)
+	    {
+	        P1OUT ^= BIT6;
+	        b = 0;
+	    }
+	}
+	return 0;
+}
