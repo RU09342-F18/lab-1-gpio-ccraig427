a2103468b8463a78e9eb8fb70e122daf1dcde692
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-19 16:11:14 -0400

    Update main.c

--- Simple Blink/MSP430F5529/main.c
+++ Simple Blink/MSP430F5529/main.c
@@ -1,1 +1,19 @@
+#include <msp430.h> 
 
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1SEL &= ~BIT0;
+	P1DIR |= BIT0;
+
+	while(1)
+	{
+	   P1OUT ^= BIT0;
+	   __delay_cycles(100000);
+	}
+	return 0;
+}
