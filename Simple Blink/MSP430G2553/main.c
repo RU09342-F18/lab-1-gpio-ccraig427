b4ff700554191bfabf7019ba8dcdec5511bbc70e
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-20 16:42:54 -0400

    Update main.c

--- Simple Blink/MSP430G2553/main.c
+++ Simple Blink/MSP430G2553/main.c
@@ -2,21 +2,21 @@
 /* 
 *File: main.c For Simple blink on MSP430G2553
 *Created On: 9/16/2018
-*Last Edited:
+*Last Edited: 9/17/2018
 *Author: Colin Craig
 */
 
 int main(void) 
 { 
-  WDTCTL = WDTPW + WDTHOLD; //Disables the watchdog timer using the WDTHOLD 
+  WDTCTL = WDTPW + WDTHOLD; //Disables the watchdog timer within the microcontroller 
   
-  P1SEL &= ~BITO; 
+  P1SEL &= ~BIT0; 
   P1DIR |= BITO0; 
   
-  while(1) 
+  while(1) //Initiate an infinite while loop 
   { 
-    P1OUT ^= BITO; 
-    __delaY_cycles(1000000); 
+    P1OUT ^= BIT0; //
+    __delaY_cycles(1000000); //Delays 1,000,000 clock cycles between each while loop 
   }
   return 0; 
 }
