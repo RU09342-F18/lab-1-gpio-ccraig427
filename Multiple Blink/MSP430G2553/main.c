2bffdf4a1a8dba9e3e7c805ea625c406300823a1
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-21 18:28:54 -0400

    Update main.c

--- Multiple Blink/MSP430G2553/main.c
+++ Multiple Blink/MSP430G2553/main.c
@@ -8,25 +8,25 @@
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR |= BIT0 | BIT6; //assigns BIT0 and BIT6 to have an output direction
-	P1SEL &= ~(BIT0 + BIT6); //assigns value of 0 to Pin1 and Pin6 in P1SEL SFR
-	int a = 0;
-	int b = 0;
+	WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+	P1DIR |= BIT0 | BIT6; //Gives P1DIR to be an output whenever BIT0 or BIT6 are 1
+	P1SEL &= ~(BIT0 + BIT6); //assigns value of 0 to Pin1.0 and Pin1.6 in P1SEL SFR
+	int a = 0; //sets counter variable to 1
+	int b = 0; //sets counter variable to 1
 	while(1)
 	{
-	    a += 1;
-	    b += 1;
-	    __delay_cycles(50000);
-	    if (a == 25)
+	    a += 1; //increments counter variable
+	    b += 1; //increments counter variable
+	    __delay_cycles(50000); //delays the while loop by 50,000 clock cycles
+	    if (a == 25) 
 	    {
-	        P1OUT ^= BIT0;
-	        a = 0;
+	        P1OUT ^= BIT0; //toggles P1.0 LED
+	        a = 0; //sets counter variable back to zero
 	    }
-	    if (b == 10)
+	    if (b == 10) 
 	    {
-	        P1OUT ^= BIT6;
-	        b = 0;
+	        P1OUT ^= BIT6; //toggles P1.6 LED 
+	        b = 0; //sets counter variable back to zero
 	    }
 	}
 	return 0;
