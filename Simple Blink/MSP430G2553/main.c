287f5025d1c0469535b04e437c9f5912ae191ce8
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-21 15:57:10 -0400

    Update main.c

--- Simple Blink/MSP430G2553/main.c
+++ Simple Blink/MSP430G2553/main.c
@@ -10,12 +10,12 @@ int main(void)
 { 
   WDTCTL = WDTPW + WDTHOLD; //Disables the watchdog timer within the microcontroller 
   
-  P1SEL &= ~BIT0; //Negates least significant bit of P1SEL
-  P1DIR |= BITO0; 
+  P1SEL &= ~BIT0; //Negates least significant bit of P1SEL register
+  P1DIR |= BITO0; //Turns on least significant bit of P1DIR register
   
   while(1) //Initiate an infinite while loop 
   { 
-    P1OUT ^= BIT0; //
+    P1OUT ^= BIT0; //Toggles least significant bit of P1OUT on and off
     __delaY_cycles(1000000); //Delays 1,000,000 clock cycles between each while loop 
   }
   return 0; 
