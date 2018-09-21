7521d0ac1e9fb5367347ab85e6a1a9f3c0f885be
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-21 18:45:07 -0400

    Update main.c

--- Multiple Blink/MSP430F5529/main.c
+++ Multiple Blink/MSP430F5529/main.c
@@ -1,1 +1,35 @@
-
+#include <msp430.h> 
+/* 
+*File: main.c For Multiple blink on MSP430F5529
+*Created On: 9/18/2018
+*Last Edited: 9/21/2018
+*Author: Colin Craig
+*/
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1DIR |= BIT0; //assigns P1.0 to have an output direction
+	P1SEL &= ~BIT0; //assigns value of 0 to Pin1 in P1SEL SFR
+	P4DIR |= BIT7; //assigns P4.7 to have an output direction 
+	P4SEL &= ~BIT7; //assigns value of 0 to Pin7 in P4SEL SFR
+	
+	int a = 0; //sets counter variable to 0
+	int b = 0;  //sets counter variable to 0
+	while(1)
+	{
+	     a += 1; //increments counter variable 
+	     b += 1; //increments counter variable
+	     __delay_cycles(50000); //delays the while loop by 50,000 clock cycles
+	     if (a == 25)
+	     {
+	         P1OUT ^= BIT0; //toggles BIT0 between 0 and 1
+	         a = 0; //resets counter variable
+	     }
+	     if (b == 10)
+	     {
+	         P4OUT ^= BIT7; //toggles BIT7 between 0 and 1 
+	         b = 0; //resets counter variable
+	     }
+	}
+	return 0;
+}
